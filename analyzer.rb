835c59b227fc4fc8df54f8150d78b89e6fe08024
Rick Thornbrugh <rthornbrug@gmail.com>
2016-11-02 00:56:51 +0000

    console ruby practice.

---
+++ analyzer.rb
@@ -0,0 +1,17 @@
+puts "Welcome to the analyzer!"
+
+puts "Please enter your first name."
+first_name = gets.chomp
+
+puts "Please enter your last name"
+last_name = gets.chomp
+
+puts "Welcome #{first_name} #{last_name}."
+
+puts "Your first name contains #{first_name.length} characters."
+
+puts "Your last name contains #{last_name.length} characters."
+
+puts "Your name reversed looks like: #{first_name.reverse} #{last_name.reverse}."
+
+
---
+++ helloworldmethod.rb
@@ -0,0 +1,7 @@
+def hello(anything)
+  puts anything
+end  
+
+hello "This is a test."
+hello "Ruby is super cool!!"
+hello "I am digging it!!"
