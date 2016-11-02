32be3366e3a47a74073109769d76a1fdec2fcfcf
Rick Thornbrugh <rthornbrug@gmail.com>
2016-11-02 06:39:21 +0000

    number practice

--- analyzer.rb
+++ analyzer.rb
@@ -14,4 +14,14 @@ puts "Your last name contains #{last_name.length} characters."
 
 puts "Your name reversed looks like: #{first_name.reverse} #{last_name.reverse}."
 
+puts "Please enter a number."
+first_number = gets.chomp
+
+puts "Please enter a second number"
+second_number = gets.chomp
+
+puts "The first number times the second number is: #{first_number.to_i * second_number.to_i}"
+puts "The first number divided by the second number is: #{first_number.to_i / second_number.to_i}"
+puts "The first number minus the second number is: #{first_number.to_i - second_number.to_i}"
+puts "The first number modulo the second number is: #{first_number.to_i % second_number.to_i}"
 
